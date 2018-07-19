0943e7fb4a24c9e48e4097d0744a2d92facc42ab
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-07-19 09:07:30 +0900

    Initial Commit

---
+++ test.server.earlgrey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test.server.earlgrey
+//
+//  Created by kazuaki matsuo on 2018/07/19.
+//  Copyright © 2018 KazuCocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
